e4167aa1088241dea24aefeb7d35e5bba69f1e80
Kobinggg <fatowtu123@gmail.com>
2025-03-08 22:31:30 +0800

    Update settings.py

--- genesis/genesis/settings.py
+++ genesis/genesis/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'rest_framework',  
     'corsheaders', 
     'rest_framework_simplejwt', 
-    'rest_framework_simplejwt.token_blacklist', 
+    'rest_framework_simplejwt.token_blacklist',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
